5ad8f449c72041ebfd40c62658c20ce38f2f094a
Hana Kim <hana.kim@pomona.edu>
2014-10-13 00:09:19 -0700

    Initial Commit

---
+++ MovingSheepPrototype/HKViewController.h
@@ -0,0 +1,13 @@
+//
+//  HKViewController.h
+//  MovingSheepPrototype
+//
+//  Created by HMC on 10/13/14.
+//  Copyright (c) 2014 Hana Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKViewController : UIViewController
+
+@end
---
+++ MovingSheepPrototype/HKViewController.m
@@ -0,0 +1,29 @@
+//
+//  HKViewController.m
+//  MovingSheepPrototype
+//
+//  Created by HMC on 10/13/14.
+//  Copyright (c) 2014 Hana Kim. All rights reserved.
+//
+
+#import "HKViewController.h"
+
+@interface HKViewController ()
+
+@end
+
+@implementation HKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
